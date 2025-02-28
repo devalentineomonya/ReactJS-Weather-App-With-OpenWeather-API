7c48e1998de5d1d8b380ea1642e3531f14392282
Valentine Omonya <valomosh254@gmail.com>
2025-03-01 00:03:59 +0300

    refactor: update global layout and CSS variables

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,15 @@ import Navbar from "@/components/common/navbar/Navbar";
 import "./globals.css";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import { ThemeProvider } from "@/components/ui/theme-provider";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "DeExpenser | Tracker Expenses",
+  description: "Track your expenses and manage your finances efficiently.",
+};
+
 const plusJakartaSans = Plus_Jakarta_Sans({ subsets: ["latin"] });
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -10,23 +18,18 @@ export default function RootLayout({
 }>) {
   return (
     <html suppressHydrationWarning lang="en">
-      <body className={` ${plusJakartaSans.className} antialiased  bg-white dark:bg-gray-800`}>
+      <body
+        className={` ${plusJakartaSans.className} antialiased  bg-white dark:bg-gray-800`}
+      >
         <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
         >
           <Navbar />
-          <main className="flex justify-center w-full ">
-            <div className="w-full max-w-[1200px]">
-
-          {children}
-            </div>
-
-          </main>
+          <main className="flex justify-center w-full ">{children}</main>
         </ThemeProvider>
-
       </body>
     </html>
   );
