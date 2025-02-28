a2bfed33c8713a4d85b02dffbe5c0c9160c4cd86
Valentine Omonya <valomosh254@gmail.com>
2025-03-01 00:04:18 +0300

    refactor: redesign home page structure

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,28 +1,28 @@
-import CurrentPrecipitation from "@/features/home/components/CurrentPrecipitation";
-import HeroBanner from "@/features/home/components/HeroBanner";
-import OtherCities from "@/features/home/widgets/OtherCities";
-import Precipitation from "@/features/home/widgets/Precipitation";
-import Pressure from "@/features/home/widgets/Pressure";
-import SummaryTable from "@/features/home/widgets/SummaryTable";
-import Temperature from "@/features/home/widgets/Temperature";
-import Trends from "@/features/home/widgets/Trends";
-import Twilight from "@/features/home/widgets/Twilight";
-import Weekly from "@/features/home/widgets/Weekly";
-import Wind from "@/features/home/widgets/Wind";
+import CurrentPrecipitation from "@/screens/home/components/CurrentPrecipitation";
+import HeroBanner from "@/screens/home/components/HeroBanner";
+import OtherCities from "@/screens/home/widgets/OtherCities";
+import Precipitation from "@/screens/home/widgets/Precipitation";
+import Pressure from "@/screens/home/widgets/Pressure";
+import SummaryTable from "@/screens/home/widgets/SummaryTable";
+import Temperature from "@/screens/home/widgets/Temperature";
+import Trends from "@/screens/home/widgets/Trends";
+import Twilight from "@/screens/home/widgets/Twilight";
+import Weekly from "@/screens/home/widgets/Weekly";
+import Wind from "@/screens/home/widgets/Wind";
 export default function Home() {
   return (
-   <main className="grid grid-cols-12 px-4 my-12">
-    <HeroBanner/>
-    <Temperature/>
-    <Precipitation/>
-    <Twilight/>
-    <Wind/>
-    <CurrentPrecipitation/>
-    <Pressure/>
-    <Weekly/>
-    <OtherCities/>
-    <Trends/>
-    <SummaryTable/>
-   </main>
+    <main className="grid grid-cols-12 px-4 my-12 max-w-[1200px] ">
+      <HeroBanner />
+      <Temperature />
+      <Precipitation />
+      <Twilight />
+      <Wind />
+      <CurrentPrecipitation />
+      <Pressure />
+      <Weekly />
+      <OtherCities />
+      <Trends />
+      <SummaryTable />
+    </main>
   );
 }
